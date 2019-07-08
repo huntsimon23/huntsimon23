b50e3ca8796cfce8e4e48a20af9813ff4923cca2
huntsimon23 <hunt.simon23@gmail.com>
2019-07-08 03:35:46 -0400

    template cleanup

--- php/config.php
+++ php/config.php
@@ -1,18 +1,15 @@
 <?
-	//define the receiver of the email
-	
-	define('TO_NAME','Simon Hunt');  //put your name
-	define('TO_EMAIL','hunt.simon23.com');  //put your email
+	define('TO_NAME','Simon Hunt');
+	define('TO_EMAIL','freshcoconutsolutions@gmail.com');
 
 	define('TEMPLATE_PATH','template/default.php');
  
 
-	define('SMTP_HOST','smtp.gmail.com'); //for example gmail: smtp.gmail.com
-	define('SMTP_USERNAME','hunt.simon23@gmail.com'); //put your email
-	define('SMTP_PASSWORD','shalom77'); //put your password
-	define('SUBJECT','Contact from your portfolio page'); //change if you want the subject
+	define('SMTP_HOST','smtp.gmail.com');
+	define('SMTP_USERNAME','freshcoconutsolutions@gmail.com');
+	define('SMTP_PASSWORD','freshcoconut');
+	define('SUBJECT','Contact from your portfolio page');
 	
-	// Messages
 	define('MSG_INVALID_NAME','Please enter your name.');
 	define('MSG_INVALID_EMAIL','Please enter valid e-mail.');
 	define('MSG_INVALID_MESSAGE','Please enter your message.');
